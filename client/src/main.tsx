461dd1442e8975b079a5167a5dda7367e70225a7
Enrique Martinez <emartinez21@hotmail.com>
2022-03-05 14:52:58 -0600

    adding chartjs routing

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,11 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import { BarElement, CategoryScale, Chart, LinearScale } from "chart.js";
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+
+Chart.register(CategoryScale, LinearScale, BarElement);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
-)
+  document.getElementById("root")
+);
