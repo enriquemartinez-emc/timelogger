735ca1c2d64c9a647376d6bc912b441f07979d59
Enrique Martinez <emartinez21@hotmail.com>
2022-03-05 14:39:12 -0600

    remove unnecessary files

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,45 +1,18 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { Outlet, Route, Routes } from "react-router-dom";
+import Layout from "./components/Layout";
+import Projects from "./views/Projects";
+import TimeRegistrations from "./views/TimeLogs";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
+    <Layout>
+      <Routes>
+        <Route path="/*" element={<Outlet />}>
+          <Route index element={<Projects />} />
+          <Route path="projects" element={<Projects />} />
+          <Route path="projects/:projectId" element={<TimeRegistrations />} />
+        </Route>
+      </Routes>
+    </Layout>
+  );
 }
-
-export default App
